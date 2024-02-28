eb0656e5b44fd1517b3c798b128a1812aa2e9f21
NAKI-94 <quina.dev94@gmail.com>
2024-02-27 23:17:41 -0500

    CAMBIOS CON CSS Y MODULADO

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
 
 import { mostrarMenu } from './moduloMenu.js';
-import {mostrarPrecio} from "./script.js";
 
-btn.addEventListener("click",mostrarPrecio);
 
-// este me salia en ayuda para corregirlo pero nada 
-btn.addEventListener("click", (event) => mostrarMenu(event));
+btn.addEventListener("click",mostrarMenu);
 
-mostrarMenu();
-mostrarPrecio();
--- moduloMenu.js
+++ moduloMenu.js
@@ -1,24 +1,24 @@
 const btn = document.querySelector("#btn");
 const menu = document.getElementById("menu"); 
-
+import {mostrarPrecio} from "./script.js";
 
 export function mostrarMenu(event) {
    event.preventDefault();
     switch(menu.selectedIndex){
     case 1:
-        mostrarPrecio("img/helado.png", "imagen de helado", "helado", "$5.000");
+        mostrarPrecio("img/helado.png", "imagen de helado", "Helado", "$5.000");
         break;
     
     case 2:
-        mostrarPrecio("img/hamburguesa.png", "imagen de hamburguesa", "helado", "$5.000");
+        mostrarPrecio("img/hamburguesa.png", "imagen de hamburguesa", "Hamburguesa", "$5.000");
         break;
     
     case 3:
-        mostrarPrecio("img/perro.png", "imagen de perro caliente", "perro caliente", "$15.000");
+        mostrarPrecio("img/perro.png", "imagen de perro caliente", "Perro caliente", "$15.000");
         break;
     
     case 4:
-        mostrarPrecio("img/ensalada.png", "imagen de ensalada", "ensalada", "$10.000");
+        mostrarPrecio("img/ensalada.png", "imagen de ensalada", "Ensalada", "$10.000");
         break;
     
     default:
